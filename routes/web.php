9b27acd951d4637846dea675943b33bf692879bf
Đức Nobi <nguyenminhduc552004@gmail.com>
2026-09-24 22:18:07 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -290,7 +290,7 @@ Route::prefix('blog')->name('client.blog.')->group(function () {
     Route::get('/', [BlogController::class, 'index'])->name('index');
     Route::get('/search', [BlogController::class, 'searchKeyword'])->name('search.keyword');
     Route::post('/api/search', [BlogController::class, 'searchApi'])->name('search.api')->middleware('throttle:30,1');
-    Route::get('/danh-muc/{category:slug}', [BlogController::class, 'category'])->name('category');
+    Route::get('/{category:slug}', [BlogController::class, 'category'])->name('category');
     Route::get('/{slug}', [BlogController::class, 'show'])->name('show');
 });
 
